6d0fcca87e302fdf30a55205d5001f0c484fd3e3
BrendaBarrios <brenda.yasmin.barrios@gmail.com>
2022-03-03 14:07:48 -0600

    Practicing getter and setters

--- src/com/company/poo/Car.java
+++ src/com/company/poo/Car.java
@@ -6,20 +6,32 @@ public class Car {
     private int heigth;
     private int width;
     private int engine;
-    private int weight;
+    private int weight_plataform;
+
+    String color;
+    int weight_total;
+    boolean leather_seats, air_condition;
+
 
     // Constructor : Contruction of sate initial of object
     public Car(){
         wheel = 4;
         heigth = 1000;
-        weight = 300;
+        weight_plataform = 300;
         engine = 1600;
         width = 600;
     }
 
     // Method getter always contain a retun
-    public String say_height(){
-        return "The height of car is :" + heigth ;
+    public String say_height(){ // Getter say of value of the variabls
+        return "The height of car is : " + heigth ;
     }
 
+    public void set_color(){ // SETTER change the value of variabls
+        color = "blue";
+    }
+
+    public String get_color(){
+        return "The color of car is : " + color;
+    }
 }
--- src/com/company/poo/Use_Car.java
+++ src/com/company/poo/Use_Car.java
@@ -1,5 +1,7 @@
 package com.company.poo;
 
+import java.sql.SQLOutput;
+
 public class Use_Car {
     public static void main(String[] args){
         // Create an objetc
@@ -7,6 +9,9 @@ public class Use_Car {
         // Instancia de clase , ejemplar de clase
         Car car = new Car();
 
+        car.set_color();
+
+        System.out.println(car.get_color());
         System.out.println(car.say_height());
     }
 }
