1edf533693ff6e504f68183180058ad40550363c
BrendaBarrios <brenda.yasmin.barrios@gmail.com>
2022-03-09 13:59:37 -0600

    Practicing use of objetcs

---
+++ src/com/company/poo/BookScore.java
@@ -0,0 +1,7 @@
+package com.company.poo;
+
+public class BookScore {
+    public void printMessage(){
+        System.out.println("Welcome the Book score");
+    }
+}
---
+++ src/com/company/poo/TestBookScore.java
@@ -0,0 +1,8 @@
+package com.company.poo;
+
+public class TestBookScore {
+    public static void main(String[] args){
+        BookScore bookScore = new BookScore();
+        bookScore.printMessage();
+    }
+}
