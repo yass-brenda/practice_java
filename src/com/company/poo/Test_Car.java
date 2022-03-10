b99da3ea307c7126f4f9b8de1344a107d20ee6ef
BrendaBarrios <brenda.yasmin.barrios@gmail.com>
2022-03-09 22:51:47 -0600

    Renaming name of file

--- src/com/company/poo/Test_Car.java
+++ src/com/company/poo/Test_Car.java
@@ -1,7 +1,7 @@
 package com.company.poo;
 import javax.swing.*;
 
-public class Use_Car {
+public class Test_Car {
     public static void main(String[] args){
         // Create an objetc
         // NameClass nameofyourobject nameofconstructor
