e111fa5b0c86d60b2caa326d259aa9c794e6a227
BrendaBarrios <brenda.yasmin.barrios@gmail.com>
2022-03-12 18:47:39 -0600

    Practicing constructor overloading

--- src/com/company/poo/UseEmployee.java
+++ src/com/company/poo/UseEmployee.java
@@ -1,39 +1,20 @@
 package com.company.poo;
+import javax.swing.plaf.synth.SynthRadioButtonMenuItemUI;
 import java.util.*; // for Date
 
 
 public class UseEmployee {
     public static void main(String[] args){
-        /*Employee employee1 = new Employee("Ulisses",120,2020,8,5);
-        Employee employee2 = new Employee("Diego",150,2020,8,7);
-        Employee employee3 = new Employee("Barrios",1200,2020,12,5);
 
-        employee1.setIncreaseSalary(5);
-        employee2.setIncreaseSalary(5);
-        employee3.setIncreaseSalary(5);
-
-        System.out.println("Name : " + employee1.getName() + " Salary : " + employee1.getSalary()
-                           + " Hight contrat: " + employee1.getDate());
-
-        System.out.println("Name : " + employee2.getName() + " Salary : " + employee2.getSalary()
-                + " Hight contrat: " + employee2.getDate());
-
-        System.out.println("Name : " + employee3.getName() + " Salary : " + employee3.getSalary()
-                + " Hight contrat: " + employee3.getDate()); */
-
-        Employee[] myEmployess = new Employee[3]; // Array of one class.
+        Employee[] myEmployess = new Employee[4]; // Array of one class.
         myEmployess[0] = new Employee("Ulisses",120,2020,8,5);
         myEmployess[1] = new Employee("Diego",150,2020,8,7);
         myEmployess[2] = new Employee("Barrios",1200,2020,12,5);
+        myEmployess[3] = new Employee("Becerra");
 
         for (Employee e: myEmployess){
             e.setIncreaseSalary(5);
         }
-
-        /* for (int i = 0;i < myEmployess.length; i++){
-            System.out.println("Name : " + myEmployess[i].getName() + " Salary : " + myEmployess[i].getSalary()
-                    + " Hight contrat: " + myEmployess[i].getDate());
-        }*/
 
         for (Employee e: myEmployess){
             System.out.println("Name : " + e.getName() + " Salary : " + e.getSalary() + " Hight contrat: " + e.getDate());
@@ -48,6 +29,12 @@ class Employee {
         salaryEmployee = salary;
         GregorianCalendar calendar = new GregorianCalendar(year,month-1,day);
         highContrat = calendar.getTime();
+    }
+
+    public Employee(String name){
+       //Constriuir objetos de los que se desconoce  algun dato y que esos datos sean iguales
+        //Ese this llama al otro constructor de la clase  si tiene smás constructores llamara al que coincida con el mismo numero de parametros
+        this(name,3000,2000,01,01);
     }
 
     public String getName(){
