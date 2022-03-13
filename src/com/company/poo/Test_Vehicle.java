b0caf2ecfe6ad17e927a28e91a98738f3fcb950d
BrendaBarrios <brenda.yasmin.barrios@gmail.com>
2022-03-13 01:25:47 -0600

    Renaming file for use in future class

---
+++ src/com/company/poo/Test_Vehicle.java
@@ -0,0 +1,17 @@
+package com.company.poo;
+
+public class Test_Vehicle {
+    public static void main(String[] args){
+        Car car1 = new Car();
+        car1.establece_color("red");
+
+        Furgoneta furgoneta = new Furgoneta(3,4);
+        furgoneta.establece_color("pink");
+        furgoneta.configure_leather_seats("si");
+        furgoneta.configureAirCondition("si");
+
+        System.out.println(car1.say_data_general() +" " + car1.get_color()  );
+
+        System.out.println(furgoneta.say_data_general() +" " + furgoneta.getDataFurgoneta() );
+    }
+}
