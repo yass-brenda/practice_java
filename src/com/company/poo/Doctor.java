8f08d94f9e2a3d39f615c81f4a0fc9713cb395ad
BrendaBarrios <brenda.yasmin.barrios@gmail.com>
2022-03-14 00:02:51 -0600

    Practicing to create objets and access a propierties and methods

---
+++ src/com/company/poo/Doctor.java
@@ -0,0 +1,15 @@
+package com.company.poo;
+
+public class Doctor {
+    int id;
+    String name;
+    String speciality;
+
+    public Doctor(String name){
+        System.out.println("Name : " +name);
+    }
+
+    public void showNae(){
+        System.out.println(name);
+    }
+}
---
+++ src/com/company/poo/TestDoctor.java
@@ -0,0 +1,9 @@
+package com.company.poo;
+
+public class TestDoctor {
+    public static void main(String[] args){
+        Doctor doctor = new Doctor("Ulisses");
+        doctor.name ="Ulisses";
+        doctor.showNae();
+    }
+}
