f9d8ab070911e1ab9e9d3c0df38fd7b4a29654a1
BrendaBarrios <brenda.yasmin.barrios@gmail.com>
2022-03-14 00:59:36 -0600

    Practicing to use static methods and propierties

--- src/com/company/poo/Doctor.java
+++ src/com/company/poo/Doctor.java
@@ -1,15 +1,20 @@
 package com.company.poo;
 
 public class Doctor {
-    int id;
+    static int id = 0; // Autoincrement
     String name;
     String speciality;
 
     public Doctor(String name){
         System.out.println("Name : " +name);
+        id++;
     }
 
-    public void showNae(){
+    public void showName(){
         System.out.println(name);
+    }
+
+    public void showId(){
+        System.out.println("Id doctor :" +id);
     }
 }
--- src/com/company/poo/TestDoctor.java
+++ src/com/company/poo/TestDoctor.java
@@ -1,9 +1,20 @@
 package com.company.poo;
+import static com.company.poo.UIMenu.*;
+
+import javax.print.Doc;
 
 public class TestDoctor {
     public static void main(String[] args){
         Doctor doctor = new Doctor("Ulisses");
         doctor.name ="Ulisses";
-        doctor.showNae();
+        doctor.showName();
+        doctor.showId();
+
+
+        Doctor myDoctorUle = new Doctor("Diego");
+        myDoctorUle.showId();
+
+        // UIMenu.showMenu();
+        showMenu();
     }
 }
