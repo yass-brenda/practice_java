c621d810a1d44bb98ec1cec7ba7d0de4579f0ef0
BrendaBarrios <brenda.yasmin.barrios@gmail.com>
2022-03-22 00:23:47 -0600

    Using a interface for employee

---
+++ src/com/company/poo/employee/Jefes.java
@@ -0,0 +1,5 @@
+package com.company.poo.employee;
+
+public interface Jefes {
+    public abstract String takeDecisions(String decision); // Delclara un método solo se definen pero no se desarollan
+}
